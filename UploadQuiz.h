f1ecab877a444a7490030fb8db2e082834f57c61
Saad Ilyas <115127978+Saad-Ilyas90@users.noreply.github.com>
2024-03-28 20:07:58 +0500

    Add files via upload

---
+++ UploadQuiz.h
@@ -0,0 +1,86 @@
+//
+// Created by basit on 12/16/2023.
+//
+
+#ifndef OOP_NEW_PROJECT_UPLOADQUIZ_H
+#define OOP_NEW_PROJECT_UPLOADQUIZ_H
+
+#include <windows.h>
+#include <commdlg.h>
+#include <fstream>
+#include"StudentEnrollCourses.h"
+
+using namespace std;
+
+class UploadQuiz {
+public:
+    void uploadQuiz(const string& enrollment) {
+        string courses[10]; // Assuming a student can enroll in a maximum of 10 courses
+        int numCourses = 0;
+
+        // Read the file to get all the enrolled courses
+        ifstream file(enrollment + "_enrolledCourses.txt");
+        string line;
+        while (getline(file, line)) {
+            if (line.find("Course Code: ") != string::npos) {
+                courses[numCourses] = line.substr(13); // Extract the course code
+                numCourses++;
+            }
+        }
+
+        // Display all the enrolled courses and ask the student to select one
+        cout << "Courses enrolled by student:\n";
+        for (int i = 0; i < numCourses; i++) {
+            cout << i + 1 << ". " << courses[i] << "\n";
+        }
+        int choice;
+        cout << "Choose a course to upload quiz: ";
+        while(!(cin >> choice) || choice < 1 || choice > numCourses) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Please enter a number between 1 and " << numCourses << ": ";
+        }
+        string courseCode = courses[choice - 1];
+
+        // Upload the quiz for the selected course
+        OPENFILENAME ofn;
+        char fileName[MAX_PATH] = "";
+        ZeroMemory(&ofn, sizeof(ofn));
+        ofn.lStructSize = sizeof(OPENFILENAME);
+        ofn.hwndOwner = NULL;
+        ofn.lpstrFilter = "All Files\0*.*\0\0";
+        ofn.lpstrFile = fileName;
+        ofn.nMaxFile = MAX_PATH;
+        ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+        ofn.lpstrDefExt = "";
+        if (GetOpenFileName(&ofn))
+        {
+            ifstream srcFile(fileName, ios::binary);
+            char buffer[MAX_PATH];
+            GetModuleFileName( NULL, buffer, MAX_PATH );
+            string::size_type pos = string( buffer ).find_last_of( "\\/" );
+            string quizPath = string( buffer ).substr( 0, pos) + "/" + enrollment + "_" + courseCode + "_Quiz"; // changed filename construction
+            ofstream dstFile(quizPath, ios::binary);
+
+            cout << "Uploading";
+            for(int i = 0; i < 5; i++) {
+                cout << ".";
+                Sleep(500); // delay for 500 milliseconds
+            }
+            cout << "\n";
+
+            dstFile << srcFile.rdbuf();
+
+            srcFile.close();
+            dstFile.close();
+
+            cout << "Quiz uploaded successfully." << endl;
+            system("pause");
+        }
+        else
+        {
+            cout << "No file selected." << endl;
+        }
+    }
+};
+#endif //OOP_NEW_PROJECT_UPLOADQUIZ_H
